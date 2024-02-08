e63ded5eacf863db110956af1eccbcd4c09e92a2
Murakami Aota <e225737@ie.u-ryukyu.ac.jp>
2024-02-08 21:54:53 +0900

    add mark

--- make_pass.py
+++ make_pass.py
@@ -2,11 +2,11 @@ import random
 import string
 
 def generate_password(length=10):
-    characters = string.ascii_letters + string.digits
+    characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
 if __name__ == "__main__":
     for _ in range(10):
         password = generate_password()
-        print("生成されたパスワードは:", password)
+        print("生成されたパスワード:", password)
