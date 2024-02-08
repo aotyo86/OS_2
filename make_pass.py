c31d442636e73a28dc34bbdc23943c8e8b93d711
Murakami Aota <e225737@ie.u-ryukyu.ac.jp>
2024-02-08 17:23:08 +0900

    2.9 pythonscript pushed

---
+++ make_pass.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length=10):
+    characters = string.ascii_letters + string.digits
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    for _ in range(10):
+        password = generate_password()
+        print("生成されたパスワードは:", password)
